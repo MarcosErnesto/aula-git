b2c3f9673f2ead3b08183264ca73d4573403d885
MarcosErnesto <mark07040461207@gmail.com>
2024-08-19 22:27:49 -0300

    Produto 2.0

--- porcentagem.js
+++ porcentagem.js
@@ -1,5 +1,10 @@
 var valor = parseFloat(prompt("Digite o valor a converter: "));
-var porcentagem = parseInt(prompt("Digite a porcentagem q deseja encontrar: "));
+var porcentagem = parseInt(prompt("Digite a porcentagem q deseja encontrar: ")); 
+
+// Converter o numero inteiro para decimal
+// só é possivel achar a % usando numero decimal
 var porcentagemDecimal = porcentagem / 100;
+//Multiplica o valor pela porcentagem para encontrar o resultado
 var resultado = valor * porcentagemDecimal;
+
 console.log("O Resultado é: " + resultado )
--- produto.js
+++ produto.js
@@ -4,5 +4,5 @@ var valorProduto = parseFloat(prompt("Qual é o valor do produto"));
 var valorTotal = quantidadeVendida * valorProduto;
 console.log("O seu valor total é de: " + valorTotal);
 var desconto = parseFloat(prompt("Qual é o seu porcento de desconto"));
-var totalDescontoemR = desconto / 100;
+var totalDescontoemR = valorTotal / 100;
 console.log("O seu desconto total é de: " + totalDescontoemR);
